347de70c511832f4cb2b9f4386ff7e27e8c4011d
Andreas Hammar <andreas.v.hammar@gmail.com>
2015-11-01 18:34:14 +0100

    Added country code support to make people match by continent

--- person.py
+++ person.py
@@ -1,9 +1,12 @@
+from incf.countryutils import transformations
+
 class Person:
-    def __init__(self, name, email, invalid_matches, amazon):
+    def __init__(self, name, email, invalid_matches, amazon, country):
         self.name = name
         self.email = email
         self.invalid_matches = invalid_matches
         self.amazon = amazon
+        self.country = transformations.cc_to_cn(country)
 
     def amazon_url(self):
         return self.amazon
